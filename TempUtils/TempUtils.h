38abd979723f47bf6a2386e22dbba301a09e59b2
WillsonGO <>
2018-08-02 16:16:21 +0800

    TempUtils v0.0.1

---
+++ TempUtils/TempUtils.h
@@ -0,0 +1,15 @@
+//
+//  TempUtils.h
+//  TempPodProject
+//
+//  Created by ZhengYi on 2018/8/2.
+//  Copyright © 2018年 ZhengYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TempUtils : NSObject
+
++ (CGFloat)getSum:(CGFloat) num1 and:(CGFloat)num2;
+
+@end
---
+++ TempUtils/TempUtils.m
@@ -0,0 +1,18 @@
+//
+//  TempUtils.m
+//  TempPodProject
+//
+//  Created by ZhengYi on 2018/8/2.
+//  Copyright © 2018年 ZhengYi. All rights reserved.
+//
+
+#import "TempUtils.h"
+
+
+@implementation TempUtils
+
++ (CGFloat)getSum:(CGFloat) num1 and:(CGFloat)num2 {
+    return num1 + num2;
+}
+
+@end
